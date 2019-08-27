9b9c930fbe4798993ed6e6708cffedb5e4c3d098
Rajaram <rr.kaliyaperumal@gmail.com>
2019-08-27 11:05:32 +0200

    Train with station logic

--- PatientCountTrain.py
+++ PatientCountTrain.py
@@ -1,58 +1,38 @@
 from rdflib import URIRef, Literal
 from SPARQLWrapper import SPARQLWrapper, JSON
 import os
-import FDP_SPARQL_crawler_RR_version
-
-
-
-# specify optional data use conditions at each FDP level
-use_conditions = [[(None,
-                    URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
-                    URIRef('http://www.re3data.org/schema/3-0#Repository')
-                    )],
-                  [(None,
-                    URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
-                    URIRef('http://www.w3.org/ns/dcat#Catalog')
-                    )],
-                  [(None,
-                    URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
-                    URIRef('http://www.w3.org/ns/dcat#Dataset')
-                    ),
-                   (None,
-                    URIRef('http://purl.org/dc/terms/license'),
-                    URIRef('http://purl.org/NET/rdflicense/MIT1.0')
-                    ),
-                   (None,
-                    URIRef('http://www.w3.org/ns/dcat#theme'),
-                    URIRef('http://dbpedia.org/resource/Disease_registry')
-                    )],
-                  [(None,
-                    URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
-                    URIRef('http://www.w3.org/ns/dcat#Distribution')
-                    ),
-                   (None,
-                    URIRef('http://www.w3.org/ns/dcat#accessURL'),
-                    None
-                    ),
-                   (None,
-                    URIRef('http://www.w3.org/ns/dcat#mediaType'),
-                   Literal('text/turtle'))
-                   ]
-                  ]
+import requests
+import rdflib
 
 
 #Get fdp uri from node envir variable
-fdp_uri = os.environ.get("DATABASE_URI")
-if fdp_uri is None:
-    fdp_uri = "http://136.243.4.200:8092/fdp"
+station_uri = os.environ.get("DATABASE_URI")
+if station_uri is None:
+    station_uri = "http://127.0.0.1:5000"
 else:
-    fdp_uri = str(fdp_uri)
+    station_uri = str(station_uri)
 
-print("fdp_uri :" + fdp_uri)
-# Init crawler
-crawler = FDP_SPARQL_crawler_RR_version.FDP_SPARQL_crawler_RR_version()
+print("station_uri :" + station_uri)
+
+# Read train requirement
+train_metadata = open('train-metadata.ttl', 'r').read()
+
 # Get SPARQL endpoint matches the condition
-sparql_endpoint = crawler.get_endpoint(URIRef(fdp_uri), use_conditions)
+url = station_uri + "/getDataAccessInterface"
+response = requests.post(url,data = train_metadata)
+
+print("content:"+ str(response.text))
+sparql_endpoint = None
+output = "No SPARQL endpoint found"
+
+if response.text and response.status_code == 200:
+    graph = rdflib.Graph()
+    graph.parse(data=response.text, format="text/turtle")
+    # Get triples store url
+    for sub, pred, obj in graph.triples( (None,  URIRef('http://www.w3.org/ns/dcat#accessURL'), None) ):
+        sparql_endpoint = str(obj)
+        break;
+    output = response.text
 
 
 
@@ -64,10 +44,10 @@ if sparql_endpoint:
     query = open('get_patient_count.rq', 'r').read()
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
-    print(results)
-    # Write query result to output file file
-    with open('output.txt', 'w') as f:
-        f.write(str(results))
-else:
-    print("No SPARQL endpoint found")
+    output = sparql.query().convert()
+
+print(output)
+# Write query result to output file file
+with open('output.txt', 'w') as f:
+    f.write(str(output))
+
